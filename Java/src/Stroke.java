cf0f37c4278369091cdb0cd6743e24f2758cffd0
Ian Loh <ianlohjy@hotmail.com>
2016-05-09 13:29:07 +1200

    Stroke points are now stored in its own StrokePoint class. Small optimisation and more accruate neighbour distance calculation for StrokePoints.

--- Java/src/Stroke.java
+++ Java/src/Stroke.java
@@ -9,14 +9,14 @@ public class Stroke {
 	AniSketch p;
 	long start_frame = 0;
 	//long current_position = 0;
-	ArrayList<PVector> points;
+	ArrayList<StrokePoint> points;
 	AnimationController a;
 	
 	Stroke(AniSketch p, AnimationController a)
 	{
 		this.p = p;
 		this.a = a;
-		points = new ArrayList<PVector>();
+		points = new ArrayList<StrokePoint>();
 	}
 	
 	public void update()
@@ -26,13 +26,13 @@ public class Stroke {
 	
 	void addPoint(float x, float y)
 	{	
-		points.add(new PVector(x,y));
+		points.add(new StrokePoint(x,y));
 	}
 
 	
 	void draw()
 	{
-		drawSkin();
+		drawInk();
 	}
 	
 	void drawLine()
@@ -44,7 +44,7 @@ public class Stroke {
 		{
 			for(int l=0; l<points.size()-1; l++)
 			{
-				p.line(points.get(l).x, points.get(l).y, points.get(l+1).x, points.get(l+1).y);
+				p.line(points.get(l).pos.x, points.get(l).pos.y, points.get(l+1).pos.x, points.get(l+1).pos.y);
 			}
 		}
 	}
@@ -56,12 +56,11 @@ public class Stroke {
 		
 		if(a.current_frame >= start_frame && a.current_frame <= start_frame+points.size()-1 )
 		{
-			p.ellipse(points.get((int)(a.current_frame-start_frame)).x, points.get((int)(a.current_frame-start_frame)).y, 20f, 20f);
+			p.ellipse(points.get((int)(a.current_frame-start_frame)).pos.x, points.get((int)(a.current_frame-start_frame)).pos.y, 20f, 20f);
 		}
 	}
 	
-	// A skin is a live computed representation of the line, that looks visually pleasing but does not neccessarily use all the stroke's raw data.
-	void drawSkin()
+	void drawInk()
 	{
 		if(points != null && points.size() > 1)
 		{
@@ -73,84 +72,34 @@ public class Stroke {
 			
 			for(int pt=0; pt<points.size()-1; pt++)
 			{
-				float avg_neighbour_dist = 0;
-				int neighbours_checked = 0;
-				int index_to_check = pt;
-				boolean checked_left = false;
-				boolean checked_right = false;
-				
-				while(!checked_left)
+				points.get(pt).updateNeighbourData();
+				float width = (2/points.get(pt).avg_neighbour_dist)*100;
+				if(width > 40)
 				{
-					index_to_check--;
-					if(index_to_check < 0 || index_to_check < pt-neighbours_to_check)
-					{
-						checked_left = true;
-						index_to_check = pt;
-						break;
-					}
-					avg_neighbour_dist += PApplet.dist(points.get(pt).x, points.get(pt).y, points.get(index_to_check).x, points.get(index_to_check).y);
-					neighbours_checked++;
-				}
-				
-				while(!checked_right)
-				{
-					index_to_check++;
-					if(index_to_check > points.size()-1 || index_to_check > pt+neighbours_to_check)
-					{
-						checked_right = true;
-						index_to_check = pt;
-						break;
-					}
-					avg_neighbour_dist += PApplet.dist(points.get(pt).x, points.get(pt).y, points.get(index_to_check).x, points.get(index_to_check).y);
-					neighbours_checked++;
-				}
-				
-				// Get the average ditance of the neighbours distance
-				if(neighbours_checked != 0)
-				{
-					avg_neighbour_dist /= neighbours_checked;
-				}
-				
-				float width = (2/avg_neighbour_dist)*100;
-				if(width > 20)
-				{
-					width = 20;
+					width = 40;
 				}
 				
 				p.noFill();
 				p.stroke(100,50);
 				p.strokeWeight(width);
-				p.line(points.get(pt).x, points.get(pt).y, points.get(pt+1).x, points.get(pt+1).y);
+				p.line(points.get(pt).pos.x, points.get(pt).pos.y, points.get(pt+1).pos.x, points.get(pt+1).pos.y);
 				
 				if(width > 10)
 				{
 					p.noStroke();
-					p.fill(100,50);
-					p.ellipse(points.get(pt).x, points.get(pt).y, width, width);
+					p.fill(100,35);
+					p.ellipse(points.get(pt).pos.x, points.get(pt).pos.y, width, width);
 				}
-				/*
-				dist_traversed += PApplet.dist(segment_start.x, segment_start.y, points.get(pt).x, points.get(pt).y);
-				
-				if(dist_traversed >= min_dist || pt == points.size()-1)
-				{
-					p.noFill();
-					p.stroke(0);
-					p.strokeWeight((num_neighbors*5) + 1 );
-					p.line(segment_start.x, segment_start.y, points.get(pt).x, points.get(pt).y);
-					
-					dist_traversed = 0;
-					segment_start = points.get(pt);
-					num_neighbors = 0;
-				}
-				else
-				{
-					num_neighbors ++; 
-				}
-				*/
 			}
 		}
 		drawCursor();
 	}
+	
+	void drawCursor2()
+	{
+		
+	}
+	
 	
 	void drawStroke()
 	{
@@ -160,12 +109,12 @@ public class Stroke {
 			{	
 				p.noStroke();
 				p.fill(0,50);
-				p.ellipse(points.get(l).x, points.get(l).y,10,10);
+				p.ellipse(points.get(l).pos.x, points.get(l).pos.y,10,10);
 				
 				p.noFill();
 				p.stroke(0,50);
 				p.strokeWeight(10);
-				p.line(points.get(l).x, points.get(l).y, points.get(l+1).x, points.get(l+1).y);
+				p.line(points.get(l).pos.x, points.get(l).pos.y, points.get(l+1).pos.x, points.get(l+1).pos.y);
 			}
 
 			drawCursor();
@@ -176,7 +125,7 @@ public class Stroke {
 	{
 		if(frame_number >= start_frame && frame_number <= start_frame+points.size()-1)
 		{
-			return points.get(frame_number-(int)start_frame);
+			return points.get(frame_number-(int)start_frame).pos;
 		}
 		else
 		{
@@ -184,17 +133,52 @@ public class Stroke {
 		}
 	}
 	
-	class Segment
+	
+	class StrokePoint
 	{
-		float width = 0;
-		float max_width = 30;
+		PVector pos;
+		int num_neighbours = 0;
+		int neighbours_to_check = 5;
+		float avg_neighbour_dist = 0;
 		
-		float start_point_index = 0;
-		float end_point_index = 0;
-		
-		Segment()
+		StrokePoint(float x, float y)
 		{
+			pos = new PVector(x,y);
+		}
+		
+		void updateNeighbourData()
+		{
+			num_neighbours = 0;
+			avg_neighbour_dist = 0;
+			// Find the current index
+			int cur_index = points.indexOf(this);
+			int check_index = cur_index;
 			
+			// Check neighbours on tbe left
+			while(true)
+			{
+				if(check_index-1 < 0 || check_index-1 < cur_index-neighbours_to_check)
+				{
+					check_index = cur_index;
+					break;	
+				}
+				avg_neighbour_dist += PApplet.dist(points.get(check_index).pos.x, points.get(check_index).pos.y, points.get(check_index-1).pos.x, points.get(check_index-1).pos.y);
+				num_neighbours++;
+				check_index--;
+			}
+			// Check neighbours on tbe right
+			while(true)
+			{
+				if(check_index+1 > points.size()-1 || check_index+1 > cur_index+neighbours_to_check)
+				{
+					check_index = cur_index;
+					break;
+				}
+				avg_neighbour_dist += PApplet.dist(points.get(check_index).pos.x, points.get(check_index).pos.y, points.get(check_index+1).pos.x, points.get(check_index+1).pos.y);
+				num_neighbours++;
+				check_index++;
+			}
+			avg_neighbour_dist /= num_neighbours;
 		}
 	}
 	
